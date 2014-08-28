a968f443cbd1130374fe9d39c48e053837d98284
Batuhan KUCUKALI <ladyarch3r@gmail.com>
2014-08-29 00:36:20 +0300

    curl followlocation problem fix

--- ajax.php
+++ ajax.php
@@ -11,7 +11,6 @@ if($_SERVER['HTTP_X_REQUESTED_WITH'] == 'XMLHttpRequest') {
 		curl_setopt($ch,CURLOPT_URL,$url);
 		curl_setopt($ch,CURLOPT_HEADER,false);
 		curl_setopt($ch,CURLOPT_RETURNTRANSFER,1);
-		curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
 		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
 		curl_setopt($ch,CURLOPT_USERAGENT,'Mozilla/5.0 (X11; U; Linux x86_64; en; rv:1.9.0.19) Gecko/20080528 Epiphany/2.22');
 		curl_setopt($ch,CURLOPT_CONNECTTIMEOUT,$timeout);
